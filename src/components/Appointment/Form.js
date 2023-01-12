bf3ee5051f1eebc4086c23cd34bf5b62f24f1c85
nawalabdi <nawalabdi03@gmail.com>
2023-01-11 21:15:50 -0500

    display appointments

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,45 @@
+import React from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+import  { useState } from 'react';
+
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+const [interviewer, setInterviewer] = useState(props.interviewer || null);
+const reset = () => {
+  setStudent("")
+ setInterviewer(null)}
+const Cancel = () => {
+  reset()
+  props.onCancel()
+}
+
+  return(<main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+      <input
+        className="appointment__create-input text--semi-bold"
+        value={student}
+        type="text"
+        onChange={(event) => setStudent(event.target.value)}
+        placeholder="Enter Student Name"
+      
+      />
+    </form>
+    <InterviewerList 
+     onChange={setInterviewer}
+     value={interviewer}
+     interviewers={props.interviewers}
+
+
+    />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger onClick={Cancel}>Cancel</Button>
+      <Button confirm onClick={() => props.onSave(student,interviewer)}>Save</Button>
+    </section>
+  </section>
+</main>)
+}
--- stories/index.js
+++ stories/index.js
@@ -15,7 +15,8 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
-
+import Form from "components/Appointment/Form";
+import { Fragment } from 'react'
 
 
 storiesOf("Button", module)
@@ -147,9 +148,12 @@ storiesOf("Button", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Appointment with Time", () => <Appointment 
+  time="12pm" />)
+  .add("Header", () => <Header 
+  time="12pm" />)
+  .add("Empty", () => <Empty 
+  onAdd={action("onAdd")}/>)
   .add("Show", ()=> <Show 
   student ="Lydia Miller-Jones" 
   interview={interviewer.name}
@@ -161,6 +165,35 @@ storiesOf("Button", module)
   onConfirm ={action("onConfirm")}
   onCancel ={action("onCancel")}
   />)
-  .add("Status", () => <Status message ="Deleting" />)
-  .add("Error", () => <Error message = "Could not delete appointment"
+  .add("Status", () => 
+  <Status message ="Deleting" />)
+  .add("Error", () =>
+   <Error message = "Could not delete appointment"
   onClose ={action("onClose")}/>)
+  .add("Edit", ()=> <Form
+  student= "Nawal"
+  interviewer={1}
+  interviewers={interviewers} 
+  onSave ={action("onSave")}
+  onCancel ={action("onCancel")}/>)
+  .add("Create", () => 
+  <Form
+  interviewers={interviewers}
+  onSave ={action("onSave")}
+  onCancel ={action("onCancel")}/>)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
